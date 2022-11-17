0b6957e8f789f95f0465f5fe2e17347e5b4f37ce
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2022-11-17 19:08:12 +0100

    test cpp program

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "Hello World!" << endl;
+    cout << "The number was " << x << endl;
+    return 0;
+}
